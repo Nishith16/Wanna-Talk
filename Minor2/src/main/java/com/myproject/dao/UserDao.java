049948efac7db95a329970089481c4f3b6520373
Nishith16 <75018137+Nishith16@users.noreply.github.com>
2021-05-10 17:21:45 +0530

    Add files via upload

---
+++ Minor2/src/main/java/com/myproject/dao/UserDao.java
@@ -0,0 +1,114 @@
+package com.myproject.dao;
+import java.sql.*;
+import com.myproject.entities.User;
+public class UserDao {
+	
+	private Connection con;
+	public UserDao(Connection con) {
+		this.con = con;
+	}
+	public boolean saveUser(User user) {
+		boolean f = false;
+		try {
+			String query = "insert into register(firstname,lastname,email,password) values (?,?,?,?)";
+			PreparedStatement pstmt = this.con.prepareStatement(query);
+			
+			pstmt.setString(1, user.getFirstname());
+			pstmt.setString(2, user.getLastname());
+			pstmt.setString(3, user.getEmail());
+			pstmt.setString(4, user.getPassword());
+			
+			pstmt.executeUpdate();
+			f=true;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return f;
+	}
+	public User getUserByEmailAndPassword(String email, String password) {
+	        User user = null;
+
+	        try {
+
+	            String query = "select * from register where email =? and password=?";
+	            PreparedStatement pstmt = con.prepareStatement(query);
+	            pstmt.setString(1, email);
+	            pstmt.setString(2, password);
+
+	            ResultSet set = pstmt.executeQuery();
+
+	            if (set.next()) {
+	                user = new User();
+
+//	             data from db
+	                String Firstname = set.getString("Firstname");
+//	             set to user object
+	                user.setFirstname(set.getString("firstname"));
+	                user.setLastname(set.getString("lastname"));
+	                user.setId(set.getInt("id"));
+	                user.setEmail(set.getString("email"));
+	                user.setPassword(set.getString("password"));
+	               
+	                user.setProfile(set.getString("profile"));
+
+	            }
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+
+	        return user;
+	    }
+	public boolean updateUser(User user) {
+
+        boolean f = false;
+        try {
+
+            String query = "update register set firstname=? ,lastname=?, email=? , password=? , profile=? where  id =?";
+            PreparedStatement p = con.prepareStatement(query);
+            p.setString(1, user.getFirstname());
+            p.setString(2, user.getLastname());
+            p.setString(3, user.getEmail());
+            p.setString(4, user.getPassword());
+            p.setString(5, user.getProfile());
+            p.setInt(6, user.getId()); 
+
+            p.executeUpdate();
+            f = true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+	
+	public User getUserByUserId(int userId) {
+        User user = null;
+        try {
+            String q = "select * from register where id=?";
+            PreparedStatement ps = this.con.prepareStatement(q);
+            ps.setInt(1, userId);
+            ResultSet set = ps.executeQuery();
+            if (set.next()) {
+                user = new User();
+
+//             data from db
+                String firstname = set.getString("firstname");
+//             set to user object
+                user.setFirstname(set.getString("firstname"));
+                user.setLastname(set.getString("lastname"));
+                user.setId(set.getInt("id"));
+                user.setEmail(set.getString("email"));
+                user.setPassword(set.getString("password"));
+                user.setProfile(set.getString("profile"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+	
+	
+}
---
+++ Minor2/src/main/java/com/myproject/entities/User.java
@@ -0,0 +1,72 @@
+package com.myproject.entities;
+import java.sql.*;
+public class User {
+	private int id;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String password;
+    private String profile;
+	
+	public User(int id, String firstname,String lastname, String email, String password) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.password = password;
+	}
+	public User() {
+		super();
+		
+	}
+	public User(String firstname,String lastname, String email, String password) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	
+	
+	
+	
+}
